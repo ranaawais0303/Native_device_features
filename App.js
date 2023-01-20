26e14375a4da35dfa283db21504f8ad8fb4c4733
Rana_awais <z.manzoor@pioneerlogics.com>
2023-01-19 23:31:26 -0800

    add screen logic to open add screen by header button

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -12,7 +13,20 @@ export default function App() {
       <StatusBar style="inverted" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
           <Stack.Screen name="AddPlace" component={AddPlace} />
         </Stack.Navigator>
       </NavigationContainer>
