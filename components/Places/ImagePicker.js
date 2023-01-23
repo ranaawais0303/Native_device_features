7d2569ef2561efc3abe67270fb78f69d1a696b50
Rana_awais <z.manzoor@pioneerlogics.com>
2023-01-23 02:29:39 -0800

    camera options with ios

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,9 +1,38 @@
 import React from "react";
-import { Button, View } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
+import { Alert, Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  // PermissionStatus,
+} from "expo-image-picker";
 
-function ImagePicker(props) {
+function ImagePicker() {
+  // const [cameraPermissionInformation, requestPermission] =
+  //   useCameraPermissions();
+
+  //For IOS
+  // async function verifyPermission() {
+  //   if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+  //     const permissionResponse = await requestPermission();
+
+  //     return permissionResponse.granted;
+  //   }
+  //   if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+  //     Alert.alert(
+  //       "Insufficient Permissions!",
+  //       "Your need to grant camera permission to use this app"
+  //     );
+  //     return false;
+  //   }
+  //   return true;
+  // }
   async function takeImageHandler() {
+    //For ios
+    // const hasPermission = await verifyPermission();
+    // if (!hasPermission) {
+    //   return;
+    // }
+    /////////////////////
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
